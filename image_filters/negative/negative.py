e2bfb532f65dd14b432c70eed77924ce5f538bfc
bayu-tri <123190015@student.upnyk.ac.id>
2022-11-20 17:33:22 +0700

    first commit

--- image_filters/negative/negative.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-#function to convert to negative image
-def negative(img):
-    return 255-img
-#function to convert to negative image using open cv
-def negative_using_openCV(img):
-    return cv2.bitwise_not(img)
